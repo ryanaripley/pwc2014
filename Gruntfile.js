e757742ec3d4841e7eba3111eff76a8f0cbcf256
Ryan A. Ripley <ryanaripley@gmail.com>
2015-01-26 10:44:20 -0600

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      build: {
+        src: ['js/libs/*.js', 'js/global.js'],
+        dest: 'js/build/global.min.js'
+      }
+    },
+    sass: {                              // Task
+      dist: {                            // Target
+        options: {                       // Target options
+          style: 'expanded'
+        },
+        files: {                                      // Dictionary of files
+          'css/global-unprefixed.css': 'scss/global.scss',       // 'destination': 'source'
+        }
+      }
+    },
+    autoprefixer: {
+      global: {
+        src: "css/global-unprefixed.css",
+        dest: "css/global.css"
+      }
+    },
+    watch: {
+      options: {
+        livereload: true,
+      },
+      scripts: {
+        files: ['js/*.js'],
+        tasks: ['uglify'],
+        options: {
+          spawn: false,
+        }
+      },
+      css: {
+        files: ['scss/*.scss'],
+        tasks: ['sass', 'autoprefixer'],
+        options: {
+          spawn: false,
+        }
+      },
+      files: {
+        files: ['*.php'],
+        options: {
+          span: false,
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify', 'sass', 'autoprefixer', 'watch']);
+
+};
---
+++ js/global.js
@@ -0,0 +1,17 @@
+// Code stolen from CSS-Tricks for smooth scrolling
+// http://css-tricks.com/snippets/jquery/smooth-scrolling/
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 400);
+        return false;
+      }
+    }
+  });
+});
